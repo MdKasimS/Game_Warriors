a104df51dec73d5b3a17fa86dc2049967a049562
Kasim Sache <itskasimsache@gmail.com>
2023-02-22 22:58:34 +0530

    This is a game where hero and monster battles.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int heroHealth, monsterHealth;
+
+heroHealth = 10;
+monsterHealth = 10;
+
+int attack;
+
+Random randomAttack = new System.Random();
+
+
+
+while(heroHealth>0 && monsterHealth>0)
+{
+    attack = randomAttack.Next()%10;
+    monsterHealth-=attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} and has now {monsterHealth} health");
+
+    if(monsterHealth>0){
+        attack = randomAttack.Next()%10;
+        heroHealth-=attack;
+        Console.WriteLine($"Hero was damaged and lost {attack} and has {heroHealth}");
+    }
+}
+
+if(heroHealth>0)
+    Console.WriteLine("Hero wins!");
+else
+    Console.WriteLine("Monster wins!");
